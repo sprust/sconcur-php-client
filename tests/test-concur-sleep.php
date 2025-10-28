b637de8526c53afdc81fb93eb2329f1c08fd8e23
spp28 <sppetrovich@mail.ru>
2025-10-29 00:00:03 +0200

    make test-concur-sleep configurable with dynamic arguments, simplify callbacks definition, and update timeout logic

--- tests/test-concur-sleep.php
+++ tests/test-concur-sleep.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+ini_set('memory_limit', '1024M');
+
 use SConcur\Entities\Context;
 use SConcur\Features\Sleep\SleepFeature;
 use SConcur\SConcur;
@@ -13,43 +15,27 @@ TestContainer::resolve();
 
 $start = microtime(true);
 
-$callbacks = [
-    function (Context $context) {
-        SleepFeature::sleep(context: $context, seconds: 1);
-    },
-    function (Context $context) {
-        SleepFeature::usleep(context: $context, microseconds: 1_000_000);
-    },
-    function (Context $context) {
-        SleepFeature::sleep(context: $context, seconds: 1);
-    },
-    function (Context $context) {
-        SleepFeature::usleep(context: $context, microseconds: 1_000_000);
-    },
-    function (Context $context) {
-        SleepFeature::sleep(context: $context, seconds: 1);
-    },
-    function (Context $context) {
-        SleepFeature::usleep(context: $context, microseconds: 1_000_000);
-    },
-    function (Context $context) {
-        SleepFeature::sleep(context: $context, seconds: 1);
-    },
-    function (Context $context) {
-        SleepFeature::usleep(context: $context, microseconds: 1_000_000);
-    },
-    function (Context $context) {
-        SleepFeature::sleep(context: $context, seconds: 1);
-    },
-    function (Context $context) {
-        SleepFeature::usleep(context: $context, microseconds: 1_000_000);
-    },
-];
+$callbacks = [];
+
+$total   = (int) ($_SERVER['argv'][1] ?? 5);
+$seconds = (int) ($_SERVER['argv'][2] ?? 1);
+$timeout = (int) ($_SERVER['argv'][3] ?? 2);
+
+echo "Total call:\t$total\n";
+echo "Seconds:\t$seconds\n";
+echo "Timeout:\t$timeout\n";
+echo "\n";
+
+foreach (range(1, $total) as $item) {
+    $callbacks[] = static function (Context $context) use ($seconds) {
+        SleepFeature::sleep(context: $context, seconds: $seconds);
+    };
+}
 
 $generator = SConcur::run(
     callbacks: $callbacks,
-    timeoutSeconds: 3,
-    limitCount: 5,
+    timeoutSeconds: $timeout,
+    limitCount: 0,
 );
 
 foreach ($generator as $result) {
